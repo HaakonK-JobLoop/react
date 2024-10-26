9800e60ba988506f1ae81b76c848556421ddf91b
HakonK-JobLoop <haakon.k@jobloop.no>
2024-10-26 14:41:56 +0200

    a

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "Mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "Luigi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "Yoshi",
+      id: 3,
+    },
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => (
+        <div className="blogPreview" key={blog.id}>
+          <h2> {blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
