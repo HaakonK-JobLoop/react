8080e7b82e700cf22b5ab21e276f40212284276c
HaakonK-JobLoop <haakon.k@jobloop.no>
2024-11-05 13:08:09 +0100

    h

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,17 +1,14 @@
-const BlogList = ({ blogs, title }) => {
-  //   const blogs = props.blogs;
-  //   const title = props.title;
-
+const BlogList = ({ blogs }) => {
   return (
-    <div className="blogList">
-      <h2>{title}</h2>
+    <div className="blog-list">
       {blogs.map((blog) => (
-        <div className="blogPreview" key={blog.id}>
-          <h2> {blog.title}</h2>
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
         </div>
       ))}
     </div>
   );
 };
+
 export default BlogList;
