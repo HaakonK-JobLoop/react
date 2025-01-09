7018734e545e44508983aeb6dcae325e03c05620
HaakonK-JobLoop <haakon.k@jobloop.no>
2025-01-09 10:23:28 +0100

    v

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -4,9 +4,11 @@ const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
+
   useEffect(() => {
+    const abortCont = new AbortController();
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then((res) => {
           if (!res.ok) {
             // error coming back from server
@@ -20,11 +22,15 @@ const useFetch = (url) => {
           setError(null);
         })
         .catch((err) => {
-          // auto catches network / connection error
-          setIsPending(false);
-          setError(err.message);
+          if (err.name === "AbortError") {
+            console.log("Fetch aborted");
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
         });
     }, 1000);
+    return () => abortCont.abort();
   }, [url]);
 
   return { data, isPending, error };
